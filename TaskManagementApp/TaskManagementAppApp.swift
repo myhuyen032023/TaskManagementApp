43838ea4d88e261d52339bb33a8898f0a2024a7d
myhuyen032023 <129963623+myhuyen032023@users.noreply.github.com>
2024-11-05 14:31:48 +0700

    Initial Commit

---
+++ TaskManagementApp/TaskManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementAppApp.swift
+//  TaskManagementApp
+//
+//  Created by Hoang Thi My Huyen on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
